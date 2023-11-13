fd6189e0cbf697bde0646d72dd9a5de9fc3e4036
Shriyansh Kumar <71579575+Shriyansh7870@users.noreply.github.com>
2023-11-13 15:33:41 +0530

    h

--- src/UserAuthencated/Login.js
+++ src/UserAuthencated/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { NavLink, useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 function Login() {
   // const navi = useNavigate();
   const [data, setData] = useState({
